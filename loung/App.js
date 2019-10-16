69a0b04d6c12cced6148c8ef70135c7b1e5fc3a8
Alec Francois <afran168@mtroyal.ca>
2019-10-16 12:37:44 -0600

    Started tutorial

--- loung/App.js
+++ loung/App.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -17,3 +18,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
+
+
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+// Import React Navigation
+import { createStackNavigator } from 'react-navigation'
+// Create the navigator
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
+});
+// Export it as the root component
+export default navigator
