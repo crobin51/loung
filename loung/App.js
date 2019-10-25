3361aea790c366b1cf2e99b59c921add22a36562
Cryston <crystonrobin@gmail.com>
2019-10-25 11:15:01 -0600

    My version of the app, some issues still to work through

--- loung/App.js
+++ loung/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+// Import React Navigation
+import { createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer} from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// Create the navigator
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
 });
+
+const App = createAppContainer(navigator);
+
+// Export it as the root component
+export default App
