6287f36684443d4375d8c5a9b1efe16ac0d2048f
Alec <afran168@mtroyal.ca>
2019-10-25 11:48:53 -0600

    Update App.js
    removed commented-out code

--- loung/App.js
+++ loung/App.js
@@ -1,26 +1,3 @@
-/*
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
-
-
 // Import the screens
 import Main from './components/Main';
 import Chat from './components/Chat';
